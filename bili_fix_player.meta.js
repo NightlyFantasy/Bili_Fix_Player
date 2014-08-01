7152e6cddfa8ae3c7321165c407a421f8f88ebfa
1005911753@qq.com <1005911753@qq.com>
2014-08-01 09:29:06 +0800

    版本3.6.5

--- bili_fix_player.meta.js
+++ bili_fix_player.meta.js
@@ -4,12 +4,14 @@
 // @description 修复B站播放器,黑科技,列表页、搜索页弹窗,破乐视限制,提供高清、低清晰源下载,弹幕下载
 // @include     /^.*\.bilibili\.(tv|com|cn)\/(video\/|search)?.*$/
 // @include     /^.*bilibili\.kankanews\.com\/(video\/|search)?.*$/
-// @version     3.6.4.1
+// @version     3.6.5
 // @updateURL   https://nightlyfantasy.github.io/Bili_Fix_Player/bili_fix_player.meta.js
 // @downloadURL https://nightlyfantasy.github.io/Bili_Fix_Player/bili_fix_player.user.js
+// @require http://static.hdslb.com/js/jquery.min.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_addStyle
+// @grant       unsafeWindow
 // @author     绯色
 // ==/UserScript==
